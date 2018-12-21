11afada0d76276153cb73554d9c88c9d8b668554
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2018-12-21 12:03:12 -0300

    Improve db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(email: 'admin@example.com', password: '123123', password_confirmation: '123123', admin: true)
 User.create(email: 'user@example.com', password: '123123', password_confirmation: '123123', admin: false)
+
+Model.create(name: 'Some weird name', value: 1, description: 'A really short description just to showcase how useless it is')
+Model.create(name: 'Model name rocks', value: 2, description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s')
+Model.create(name: 'Running out names', value: 3, description: 'It has survived not only five centuries, but also the leap into electronic typesetting')
+Model.create(name: 'Ok - I still got some', value: 4, description: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages')
+Model.create(name: 'Done that\'s it', value: 5, description: 'Contrary to popular belief, Lorem Ipsum is not simply random text')
